42d10567b1dfe28965fa25f85b6c65baa885fc8e
Brian Lee <tzhlee@gmail.com>
2019-05-18 15:15:16 -0500

    Post &alert completed

--- src/app.js
+++ src/app.js
@@ -4,10 +4,34 @@ import {ui} from './ui';
 //Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+//Listen for add post 
+document.querySelector('.post-submit').addEventListener('click', SubmitPost);
 
-
+//Get Posts
 function getPosts() {
     http.get('http://localhost:3000/posts')
         .then(data => ui.showPosts(data))
+        .catch(err => console.log(err));
+}
+
+//Submit Post
+function SubmitPost() {
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+
+    //ES2015 syntax
+    
+    const data = {
+        title, //title: title
+        body //body: body
+    }
+
+    //Create Post
+    http.post('http://localhost:3000/posts', data)
+        .then(data => {
+            ui.showAlert('Post added', 'alert alert-success');
+            ui.clearFields();
+            getPosts();
+        })
         .catch(err => console.log(err));
 }
